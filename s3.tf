a06cafe9f0cd7f6458fcc8c5d8ce63de4b52079f
gabin-sime <gabin75@yahoo.de>
2024-01-03 02:56:51 -0600

    modified s3.tf file

--- s3.tf
+++ s3.tf
@@ -1,14 +1,19 @@
 #create s3 bucket
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
-  versioning {
-    enabled = true
+
+}
+
+resource "aws_s3_bucket_versioning" "mybucket-versioning" {
+  bucket = aws_s3_bucket.mybucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  acl = var.acl
 }
 
 
-resource "aws_s3_bucket_ownership_controls" "example" {
+
+resource "aws_s3_bucket_ownership_controls" "mybucket-own-contr" {
   bucket = aws_s3_bucket.mybucket.id
 
   rule {
@@ -16,7 +21,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_public_access_block" "mybucket-pab" {
   bucket = aws_s3_bucket.mybucket.id
 
   block_public_acls       = false
@@ -25,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "mybucket-acl" {
   depends_on = [
     aws_s3_bucket_ownership_controls.example,
     aws_s3_bucket_public_access_block.example,
@@ -77,5 +82,5 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 
-  depends_on = [ aws_s3_bucket_acl.example.id ]
+  depends_on = [ aws_s3_bucket_acl.mybucket-acl.id ]
 }
