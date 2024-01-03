ce0cae5919ac5cdab5b2aa7623bcc7a666fae0a4
Gabin Sime <115108532+Gabinsime75@users.noreply.github.com>
2024-01-03 02:36:34 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,11 @@
 #create s3 bucket
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
+  versioning {
+    enabled = true
+  }
+  acl = var.acl
+}
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
