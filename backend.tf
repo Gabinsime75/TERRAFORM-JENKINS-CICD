0e06b483d78f266e20c4101f0d4828a410e7ae83
Gabin Sime <115108532+Gabinsime75@users.noreply.github.com>
2024-01-03 18:30:24 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "proj01-bucket1"
+    bucket         = "proj1-bucket1"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "proj1-dynamo-db-table"
